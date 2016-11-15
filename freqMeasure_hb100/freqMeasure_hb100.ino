16e46f355b6f231b33f8bdfc12b6924eccfed899
Mikael Andersson <mikael.andersson@volumental.com>
2016-11-15 21:08:48 +0100

    added lcd

--- freqMeasure_hb100/freqMeasure_hb100.ino
+++ freqMeasure_hb100/freqMeasure_hb100.ino
@@ -1,15 +1,26 @@
-/* FreqMeasure - Example with serial output
- * http://www.pjrc.com/teensy/td_libs_FreqMeasure.html
- *
- * This example code is in the public domain.
- */
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+#include <FreqMeasure.h>
+#include <Wire.h>
+#include <LCD.h>
+#include <LiquidCrystal_I2C.h>
 
 #define DOPPLER_SHIFT_CONSTANT 19.49
-#include <FreqMeasure.h>
+#define LCD_ADDRESS 0x27
+ 
+LiquidCrystal_I2C  lcd(LCD_ADDRESS,2,1,0,4,5,6,7);
 
 void setup() {
   Serial.begin(57600);
   FreqMeasure.begin();
+  // activate LCD module
+  lcd.begin (16,2); // for 16 x 2 LCD module
+  lcd.setBacklightPin(3,POSITIVE);
+  lcd.setBacklight(HIGH);
+  lcd.home(); // set cursor to 0,0
+  lcd.print("Speed radar:"); 
+  lcd.setCursor (0,1);        // go to start of 2nd line
+  lcd.print("Started"); 
 }
 
 double sum=0;
@@ -22,7 +33,9 @@ void loop() {
     count = count + 1;
     if (count > 30) {
       float frequency = FreqMeasure.countToFrequency(sum / count);
-      Serial.println(frequency/DOPPLER_SHIFT_CONSTANT);
+      lcd.setCursor (0,1);        // go to start of 2nd line
+      lcd.print(frequency/DOPPLER_SHIFT_CONSTANT);
+      lcd.print(" km/h");      
       sum = 0;
       count = 0;
     }
