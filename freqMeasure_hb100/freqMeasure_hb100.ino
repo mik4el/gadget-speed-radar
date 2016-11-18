e9020ade167b34e50a7011660742392c04bbd828
Mikael Andersson <mikael.andersson@volumental.com>
2016-11-18 10:14:53 +0100

    ui fixes

--- freqMeasure_hb100/freqMeasure_hb100.ino
+++ freqMeasure_hb100/freqMeasure_hb100.ino
@@ -7,7 +7,8 @@
 
 #define DOPPLER_SHIFT_CONSTANT 19.49
 #define LCD_ADDRESS 0x27
- 
+#define CLEAN_SCREEN_INTERVAL 5000L
+#define MINIMUM_SPEED 0.3 
 LiquidCrystal_I2C  lcd(LCD_ADDRESS,2,1,0,4,5,6,7);
 
 void setup() {
@@ -23,22 +24,39 @@ void setup() {
   lcd.print("Started"); 
 }
 
-double sum=0;
-int count=0;
+double freq_sum = 0;
+int freq_count = 0;
+unsigned long time_since_reading = 0;
 
 void loop() {
   if (FreqMeasure.available()) {
+    
     // average several reading together
-    sum = sum + FreqMeasure.read();
-    count = count + 1;
-    if (count > 30) {
-      float frequency = FreqMeasure.countToFrequency(sum / count);
+    freq_sum = freq_sum + FreqMeasure.read();
+    freq_count++;
+    
+    unsigned long currentMillis = millis();
+
+    // print speed when enough samples
+    if (freq_count > 30) {
+      float frequency = FreqMeasure.countToFrequency(freq_sum / freq_count);
       lcd.setCursor (0,1);        // go to start of 2nd line
-      lcd.print(frequency/DOPPLER_SHIFT_CONSTANT);
-      lcd.print(" km/h");      
-      sum = 0;
-      count = 0;
+      float speed = frequency/DOPPLER_SHIFT_CONSTANT;
+      if (speed > MINIMUM_SPEED) {
+        lcd.print(speed);
+        lcd.print(" km/h     ");      
+        time_since_reading = currentMillis;
+      } 
+      freq_sum = 0;
+      freq_count = 0;
     }
+    
+    // show no speed if no new reading in last 5s
+    if (currentMillis - time_since_reading >= CLEAN_SCREEN_INTERVAL) {
+      lcd.setCursor (0,1);        // go to start of 2nd line
+      lcd.print("...        ");
+      time_since_reading = currentMillis;      
+    }
+    
   }
 }
-
